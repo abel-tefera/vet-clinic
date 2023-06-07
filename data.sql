03fceabb2891762c0617e8b34f919f146b2f8507
abel-tefera <abeltefera16@gmail.com>
2023-06-07 10:32:45 +0300

    Inserted more data

--- data.sql
+++ data.sql
@@ -5,3 +5,12 @@ VALUES (1, 'Agumon', '2020-03-02', 0, true, 10.23),
 (2, 'Gabumon', '2018-11-15', 2, true, 8),
 (3, 'Pikachu', '2021-01-07', 1, false, 15.04),
 (4, 'Devimon', '2017-05-12', 5, true,11);
+
+
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (5, 'Charmander', '2-8-2020',-11,'0',0);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (6, 'Plantmon', '11-15-2021',-5.7,'1',2);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (7, 'Squirtle', '4-2-1993',-12.13,'0',3);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (8, 'Angemon', '6-12-2005',-45,'1',1);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (9, 'Boarmon', '6-7-2005',20.4,'1',7);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (10, 'Blossom', '10-13-1998',17,'1',3);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (11, 'Ditto', '5-14-2022',22,'1',4);
