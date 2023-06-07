627e867c36f54464f30852635d2fc8a2202e44ed
abel-tefera <abeltefera16@gmail.com>
2023-06-07 10:33:45 +0300

    transactions created

--- queries.sql
+++ queries.sql
@@ -16,3 +16,59 @@ SELECT * FROM animals where neutered =true;
 SELECT * FROM animals where name!='Gabumon' ;
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals where weight_kg>=10.4 AND weight_kg<=17.3;
+
+
+BEGIN;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species is null;
+
+-- Commit the transaction.
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals WHERE 1=1;
+
+ROLLBACK;
+
+
+BEGIN; 
+
+-- Delete all animals born after Jan 1st, 2022.
+
+DELETE From animals
+WHERE date_of_birth > '01-01-2022';
+
+-- Create a savepoint for the transaction.
+
+SAVEPOINT dob_jan_2022;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+
+ROLLBACK TO dob_jan_2022;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- Commit transaction
+
+COMMIT;
